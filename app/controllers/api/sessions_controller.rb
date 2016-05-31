5bc251dc843038d986cf6c23781927b2c1142f56
Alex Poe <poealext@gmail.com>
2016-05-31 14:05:24 -0400

    Creates sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,43 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render(
+        json: {
+          base: ["Incorrect username/password"]
+        }
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render(
+        json: {
+          base: ["Nobody is signed in!"]
+        },
+        status: 404
+      )
+    end
+  end
+
+  def show
+    if current_user
+      @user = current_user
+      render "api/users/show"
+    else
+      render json: {}
+    end
+  end
+end
