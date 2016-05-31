a3faeb74695b6e9b948f35b4996b71899376aa89
Alex Poe <poealext@gmail.com>
2016-05-31 14:33:52 -0400

    Creates dispatcher and router

---
+++ frontend/upwardDog.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
+
+var Router = (
+  <Router history={ hashHistory }>
+    <Route>
+
+    </Route>
+  </Router>
+);
