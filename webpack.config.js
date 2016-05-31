579d46da69068cd75897b13466acddf10a3a0721
Alex Poe <poealext@gmail.com>
2016-05-31 12:22:08 -0400

    Migrates users table

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/asana.jsx",
+  entry: "./frontend/upwardDog.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
