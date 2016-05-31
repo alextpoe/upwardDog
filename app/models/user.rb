0dc9992271cb488ce206201ae11159aea5ee4c64
Alex Poe <poealext@gmail.com>
2016-05-31 17:30:32 -0400

    another fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :username, :password_digest, :session_token, presence: true
   validates :username, uniqueness: true,
-  validates (:password, length: {minimum: 6, allow_nil: true})
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   after_initialize :ensure_session_token
   before_validation :ensure_session_token_uniqueness
