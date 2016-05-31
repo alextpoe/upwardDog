08f4385fab3d489a031244c52e0fc1f3d1d6eaeb
Alex Poe <poealext@gmail.com>
2016-05-31 17:32:56 -0400

    another fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_reader :password
 
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, uniqueness: true,
+  validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   after_initialize :ensure_session_token
